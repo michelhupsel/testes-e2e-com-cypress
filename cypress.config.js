cfb735bc4eabbea9c196db02748414772a17fbe8
michelhupsel <michelhupsel@gmail.com>
2023-08-23 17:29:16 -0300

    adicionando tag no teste de logout

--- cypress.config.js
+++ cypress.config.js
@@ -8,8 +8,10 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768,
     },
-    setupNodeEvents(/*on, config*/) {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@cypress/grep/src/plugin')(config)
+      return config
     },
   },
   projectId: '857m3o'
