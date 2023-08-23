ce0224084ac832afbfd63823485f07c749e03981
michelhupsel <michelhupsel@gmail.com>
2023-08-23 14:30:20 -0300

    criando os testes end2end

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,17 @@
+describe('Login', () => {
+  it('successfully logs in', () => {
+    cy.guiLogin()
+  })
+})
+
+// describe('Login', () => {
+//   it('Testa funcionalidade login', () => {
+//     cy.visit('/login')
+//     cy.get('#email').type(Cypress.env('USER_EMAIL'))
+//     cy.get('#password').type(Cypress.env('USER_PASSWORD'), { log: false })
+//     cy.get('.LoaderButton').should('contain', 'Login').click()
+
+//     cy.contains('h1', 'Your Notes').should('be.visible')
+//     cy.contains('a', 'Create a new note').should('be.visible')
+//   })
+// })
